4a6eae05d823d7e382a130a7a8c54f4579016935
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-28 16:35:26 +0300

    give table column names names strting with small characters

--- db/migrate/20220927122307_create_likes.rb
+++ db/migrate/20220927122307_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :AuthorId, null: false, foreign_key: true
-      t.references :PostId, null: false, foreign_key: true
+      t.references :author_id, null: false, foreign_key: true
+      t.references :post_id, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220927122430_create_comments.rb
+++ db/migrate/20220927122430_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :AuthorId, null: false, foreign_key: true
-      t.references :PostId, null: false, foreign_key: true
+      t.references :author_id, null: false, foreign_key: true
+      t.references :post_id, null: false, foreign_key: true
       t.text :Text
 
       t.timestamps
