9ecbebfb82ada75fc91197b78481f760dcc34596
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-28 16:54:13 +0300

    finalizing migration files

--- db/migrate/20220927122430_create_comments.rb
+++ db/migrate/20220927122430_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author_id, null: false, foreign_key: true
-      t.references :post_id, null: false, foreign_key: true
-      t.text :Text
+      t.references :author_id, null: false, foreign_key: {to_table: :users}
+      t.references :post_id, null: false, foreign_key: {to_table: :posts}
+      t.text :text
 
       t.timestamps
     end
