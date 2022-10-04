1ee1e26d144842fed672d509f04113b26bc45963
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-04 22:44:01 +0300

    Prefer single quotes to double quotes

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   it 'Shows the status of the request' do
     get('/users/1/posts/1')
     expect(response).to have_http_status(200)
