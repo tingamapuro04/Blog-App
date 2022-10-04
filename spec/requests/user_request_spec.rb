20ad64e81db47aee5da27de001530225f25e1dba
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-04 22:45:32 +0300

    Prefer single quotes to double quotes

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   it 'Get the status of the response' do
     get('/users/index')
     expect(response).to have_http_status(200)
