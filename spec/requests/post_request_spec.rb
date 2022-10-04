b8f7d14b5daaf0b96641fceac7d9ae1b2add2ef8
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-04 22:22:23 +0300

    Add test for users controller

--- spec/requests/post_request_spec.rb
+++ spec/requests/post_request_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
-# RSpec.describe "Posts", type: :request do
-# end
+RSpec.describe "Posts", type: :request do
+  it "Shows the status of the request" do
+    get('/users/1/posts')
+    expect(response).to have_http_status(200)
+  end
+
+  it 'Shows the sample text on view' do
+    get('/users/1/posts')
+    expect(response.body).to include 'I am a post'
+  end
+
+  it 'Renders the correct view' do
+    
+  end
+
+end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
-# RSpec.describe "Users", type: :request do
-# end
+RSpec.describe "Users", type: :request do
+  it 'Get the status' do
+  get("/users/show")
+
+  expect(response).to have_http_status(200)
+  end
+
+  it 'Shows the output on the view' do
+    get("/users/show")
+    expect(response.body).to include 'I am a list of users'
+  end
+
+  it 'Render the correct view' do
+    get("/users/show")
+    expect(response).to render_template('show')
+  end
+  
+  
+end
