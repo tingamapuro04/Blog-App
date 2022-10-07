1131f17b4df4d85f72e75b7f30de99f15eccc7c2
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-07 14:37:09 +0300

    save posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :show]
+    resources :posts, only: [:index, :new, :create, :show]
   end
   
   # root 'users#show'
