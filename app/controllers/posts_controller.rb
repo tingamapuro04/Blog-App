8d393908ac28c589124a5fc4e2d227ed566175b0
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-07 11:51:59 +0300

    add route for the new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,14 @@ class PostsController < ApplicationController
     @posts = @user2.posts
   end
 
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+
+  end
+
   def show
     @posta = Post.find params[:id]
   end
