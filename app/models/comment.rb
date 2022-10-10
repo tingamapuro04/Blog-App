584bf00fd868e3a3844b1ba34c2c2750d8e62aa3
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-10 21:15:19 +0300

    add post and comments to database

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
-  validates :text, length: { minimum: 1 }
+  # validates :text, length: { minimum: 1 }
 
   after_save :update_comments_counter
 
