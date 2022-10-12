5c55a74a87dd0ffe0dd934cf1cd69381ce731341
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-12 17:45:43 +0300

    Correct path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def new
-    @comment = Comment.new.includes([:author_id])
+    @comment = Comment.new
     respond_to do |format|
       format.html { render :new, locals: { comment: @comment } }
     end
