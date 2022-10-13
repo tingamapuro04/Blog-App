15e54d8ca6db55d56809b472380e86e96b94d199
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-13 12:01:58 +0300

    Remove role column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_085240) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_090105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,7 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_085240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "posts_counter"
-    t.string "role"
   end
 
   add_foreign_key "comments", "posts"
