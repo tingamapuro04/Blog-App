662de20da774ae79a82bd7016180b950895ae491
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-13 11:13:57 +0300

    Remove already existing migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
   # validates :name, presence: true
   # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  Roles = %w{admin default}
+
+  Roles.each do |role_name|
+    define_method "#{role_name}?" do
+      role == role_name
+    end
+  end
+
   def recent_posts
     posts.order('created_at Desc').limit(3)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_112106) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_233637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_112106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "posts_counter"
+    t.string "role"
   end
 
   add_foreign_key "comments", "posts"
